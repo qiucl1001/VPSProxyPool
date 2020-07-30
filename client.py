5c045da2f17572460780d79f041da2794176a2ff
qiucl1001 <qcl0306777@163.com>
2020-07-30 19:17:03 +0800

    vps拨号宿主机启动脚本

---
+++ client.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+# author: QCL
+# software: PyCharm Professional Edition 2018.2.8
+from vps.get_dynamic_ip import Getter
+
+
+def main():
+    g = Getter()
+    g.run()
+
+
+if __name__ == '__main__':
+    main()
+
+
